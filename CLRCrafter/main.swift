8a7dc43dd5924b67c71212d0f7baa7499ced462a
Alex Antonyuk <alex@antonyuk.me>
2016-11-08 13:19:59 +0200

    Initial Commit

---
+++ CLRCrafter/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CLRCrafter
+//
+//  Created by Alex Antonyuk on 11/8/16.
+//  Copyright © 2016 Alex Antonyuk. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
